709df8cbce46e32f13990cf16daa27cad55a3df0
Ankit Karnany <ankitkarnany@gmail.com>
2024-02-24 19:24:50 +0100

    refact: naive ui dependency removed

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # whimsy-editor-vue
+
 Whimsy is a Notion-style WYSIWYG editor with AI-powered autocompletion.
 
 ## whimsy-editor-vue
+
 Whimsy Editor is built on the following stack:
 
 - [Nuxt3](https://nuxt.com/) – framework
 - [Tiptap](https://tiptap.dev/) – text editor
 - [OpenAI](https://openai.com/) - AI completions
 - [Vercel](https://sdk.vercel.ai/docs) AI SDK – AI library
-- [NaiveUI](https://www.naiveui.com/en-US/os-theme) – styles
--- nuxt-server/app.vue
+++ nuxt-server/app.vue
@@ -1,8 +1,28 @@
 <template>
-  <Editor />
+  <div class="demo">
+    <div class="demo-container">
+      <Editor completion-api="http://localhost:3000/api/generate" />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { Editor } from "whimsy-editor-vue";
 import "whimsy-editor-vue/dist/style.css";
 </script>
+
+<style scoped lang="scss">
+.demo {
+  padding: 20vh 2rem;
+
+  &-container {
+    width: 100%;
+    max-width: 1024px;
+    margin: auto;
+  }
+
+  @media (max-width: 480px) {
+    padding: 4vh 2rem;
+  }
+}
+</style>
